052f4bb4e286d699a08fbffbf78b86143735c279
Ido Kronik <ido.kronik@mail.huji.ac.il>
2025-04-25 19:02:56 +0300

    write slave side

--- camera module/main/camera_pins.h
+++ camera module/main/camera_pins.h
@@ -1,162 +1,6 @@
-#if defined(CAMERA_MODEL_WROVER_KIT)
-#define PWDN_GPIO_NUM  -1
-#define RESET_GPIO_NUM -1
-#define XCLK_GPIO_NUM  21
-#define SIOD_GPIO_NUM  26
-#define SIOC_GPIO_NUM  27
+#pragma once
+#include "esp_camera.h"
 
-#define Y9_GPIO_NUM    35
-#define Y8_GPIO_NUM    34
-#define Y7_GPIO_NUM    39
-#define Y6_GPIO_NUM    36
-#define Y5_GPIO_NUM    19
-#define Y4_GPIO_NUM    18
-#define Y3_GPIO_NUM    5
-#define Y2_GPIO_NUM    4
-#define VSYNC_GPIO_NUM 25
-#define HREF_GPIO_NUM  23
-#define PCLK_GPIO_NUM  22
-
-#elif defined(CAMERA_MODEL_ESP_EYE)
-#define PWDN_GPIO_NUM  -1
-#define RESET_GPIO_NUM -1
-#define XCLK_GPIO_NUM  4
-#define SIOD_GPIO_NUM  18
-#define SIOC_GPIO_NUM  23
-
-#define Y9_GPIO_NUM    36
-#define Y8_GPIO_NUM    37
-#define Y7_GPIO_NUM    38
-#define Y6_GPIO_NUM    39
-#define Y5_GPIO_NUM    35
-#define Y4_GPIO_NUM    14
-#define Y3_GPIO_NUM    13
-#define Y2_GPIO_NUM    34
-#define VSYNC_GPIO_NUM 5
-#define HREF_GPIO_NUM  27
-#define PCLK_GPIO_NUM  25
-
-#define LED_GPIO_NUM 22
-
-#elif defined(CAMERA_MODEL_M5STACK_PSRAM)
-#define PWDN_GPIO_NUM  -1
-#define RESET_GPIO_NUM 15
-#define XCLK_GPIO_NUM  27
-#define SIOD_GPIO_NUM  25
-#define SIOC_GPIO_NUM  23
-
-#define Y9_GPIO_NUM    19
-#define Y8_GPIO_NUM    36
-#define Y7_GPIO_NUM    18
-#define Y6_GPIO_NUM    39
-#define Y5_GPIO_NUM    5
-#define Y4_GPIO_NUM    34
-#define Y3_GPIO_NUM    35
-#define Y2_GPIO_NUM    32
-#define VSYNC_GPIO_NUM 22
-#define HREF_GPIO_NUM  26
-#define PCLK_GPIO_NUM  21
-
-#elif defined(CAMERA_MODEL_M5STACK_V2_PSRAM)
-#define PWDN_GPIO_NUM  -1
-#define RESET_GPIO_NUM 15
-#define XCLK_GPIO_NUM  27
-#define SIOD_GPIO_NUM  22
-#define SIOC_GPIO_NUM  23
-
-#define Y9_GPIO_NUM    19
-#define Y8_GPIO_NUM    36
-#define Y7_GPIO_NUM    18
-#define Y6_GPIO_NUM    39
-#define Y5_GPIO_NUM    5
-#define Y4_GPIO_NUM    34
-#define Y3_GPIO_NUM    35
-#define Y2_GPIO_NUM    32
-#define VSYNC_GPIO_NUM 25
-#define HREF_GPIO_NUM  26
-#define PCLK_GPIO_NUM  21
-
-#elif defined(CAMERA_MODEL_M5STACK_WIDE)
-#define PWDN_GPIO_NUM  -1
-#define RESET_GPIO_NUM 15
-#define XCLK_GPIO_NUM  27
-#define SIOD_GPIO_NUM  22
-#define SIOC_GPIO_NUM  23
-
-#define Y9_GPIO_NUM    19
-#define Y8_GPIO_NUM    36
-#define Y7_GPIO_NUM    18
-#define Y6_GPIO_NUM    39
-#define Y5_GPIO_NUM    5
-#define Y4_GPIO_NUM    34
-#define Y3_GPIO_NUM    35
-#define Y2_GPIO_NUM    32
-#define VSYNC_GPIO_NUM 25
-#define HREF_GPIO_NUM  26
-#define PCLK_GPIO_NUM  21
-
-#define LED_GPIO_NUM 2
-
-#elif defined(CAMERA_MODEL_M5STACK_ESP32CAM)
-#define PWDN_GPIO_NUM  -1
-#define RESET_GPIO_NUM 15
-#define XCLK_GPIO_NUM  27
-#define SIOD_GPIO_NUM  25
-#define SIOC_GPIO_NUM  23
-
-#define Y9_GPIO_NUM    19
-#define Y8_GPIO_NUM    36
-#define Y7_GPIO_NUM    18
-#define Y6_GPIO_NUM    39
-#define Y5_GPIO_NUM    5
-#define Y4_GPIO_NUM    34
-#define Y3_GPIO_NUM    35
-#define Y2_GPIO_NUM    17
-#define VSYNC_GPIO_NUM 22
-#define HREF_GPIO_NUM  26
-#define PCLK_GPIO_NUM  21
-
-#elif defined(CAMERA_MODEL_M5STACK_UNITCAM)
-#define PWDN_GPIO_NUM  -1
-#define RESET_GPIO_NUM 15
-#define XCLK_GPIO_NUM  27
-#define SIOD_GPIO_NUM  25
-#define SIOC_GPIO_NUM  23
-
-#define Y9_GPIO_NUM    19
-#define Y8_GPIO_NUM    36
-#define Y7_GPIO_NUM    18
-#define Y6_GPIO_NUM    39
-#define Y5_GPIO_NUM    5
-#define Y4_GPIO_NUM    34
-#define Y3_GPIO_NUM    35
-#define Y2_GPIO_NUM    32
-#define VSYNC_GPIO_NUM 22
-#define HREF_GPIO_NUM  26
-#define PCLK_GPIO_NUM  21
-
-#elif defined(CAMERA_MODEL_M5STACK_CAMS3_UNIT)
-#define PWDN_GPIO_NUM  -1
-#define RESET_GPIO_NUM 21
-#define XCLK_GPIO_NUM  11
-#define SIOD_GPIO_NUM  17
-#define SIOC_GPIO_NUM  41
-
-#define Y9_GPIO_NUM    13
-#define Y8_GPIO_NUM    4
-#define Y7_GPIO_NUM    10
-#define Y6_GPIO_NUM    5
-#define Y5_GPIO_NUM    7
-#define Y4_GPIO_NUM    16
-#define Y3_GPIO_NUM    15
-#define Y2_GPIO_NUM    6
-#define VSYNC_GPIO_NUM 42
-#define HREF_GPIO_NUM  18
-#define PCLK_GPIO_NUM  12
-
-#define LED_GPIO_NUM 14
-
-#elif defined(CAMERA_MODEL_AI_THINKER)
 #define PWDN_GPIO_NUM  32
 #define RESET_GPIO_NUM -1
 #define XCLK_GPIO_NUM  0
@@ -178,160 +22,46 @@
 // 4 for flash led or 33 for normal led
 #define LED_GPIO_NUM   4
 
-#elif defined(CAMERA_MODEL_TTGO_T_JOURNAL)
-#define PWDN_GPIO_NUM  0
-#define RESET_GPIO_NUM 15
-#define XCLK_GPIO_NUM  27
-#define SIOD_GPIO_NUM  25
-#define SIOC_GPIO_NUM  23
+camera_config_t getCameraConfig() {
+  camera_config_t config;
+  config.ledc_channel = LEDC_CHANNEL_0;
+  config.ledc_timer = LEDC_TIMER_0;
+  config.pin_d0 = Y2_GPIO_NUM;
+  config.pin_d1 = Y3_GPIO_NUM;
+  config.pin_d2 = Y4_GPIO_NUM;
+  config.pin_d3 = Y5_GPIO_NUM;
+  config.pin_d4 = Y6_GPIO_NUM;
+  config.pin_d5 = Y7_GPIO_NUM;
+  config.pin_d6 = Y8_GPIO_NUM;
+  config.pin_d7 = Y9_GPIO_NUM;
+  config.pin_xclk = XCLK_GPIO_NUM;
+  config.pin_pclk = PCLK_GPIO_NUM;
+  config.pin_vsync = VSYNC_GPIO_NUM;
+  config.pin_href = HREF_GPIO_NUM;
+  config.pin_sccb_sda = SIOD_GPIO_NUM;
+  config.pin_sccb_scl = SIOC_GPIO_NUM;
+  config.pin_pwdn = PWDN_GPIO_NUM;
+  config.pin_reset = RESET_GPIO_NUM;
+  config.xclk_freq_hz = 20000000;
+  config.frame_size = FRAMESIZE_SXGA;
+  // config.frame_size = FRAMESIZE_UXGA;
+  config.pixel_format = PIXFORMAT_JPEG;  // for streaming
+  config.grab_mode = CAMERA_GRAB_WHEN_EMPTY;
+  config.fb_location = CAMERA_FB_IN_PSRAM;
+  config.jpeg_quality = 12;
+  config.fb_count = 1;
 
-#define Y9_GPIO_NUM    19
-#define Y8_GPIO_NUM    36
-#define Y7_GPIO_NUM    18
-#define Y6_GPIO_NUM    39
-#define Y5_GPIO_NUM    5
-#define Y4_GPIO_NUM    34
-#define Y3_GPIO_NUM    35
-#define Y2_GPIO_NUM    17
-#define VSYNC_GPIO_NUM 22
-#define HREF_GPIO_NUM  26
-#define PCLK_GPIO_NUM  21
-
-#elif defined(CAMERA_MODEL_XIAO_ESP32S3)
-#define PWDN_GPIO_NUM  -1
-#define RESET_GPIO_NUM -1
-#define XCLK_GPIO_NUM  10
-#define SIOD_GPIO_NUM  40
-#define SIOC_GPIO_NUM  39
-
-#define Y9_GPIO_NUM    48
-#define Y8_GPIO_NUM    11
-#define Y7_GPIO_NUM    12
-#define Y6_GPIO_NUM    14
-#define Y5_GPIO_NUM    16
-#define Y4_GPIO_NUM    18
-#define Y3_GPIO_NUM    17
-#define Y2_GPIO_NUM    15
-#define VSYNC_GPIO_NUM 38
-#define HREF_GPIO_NUM  47
-#define PCLK_GPIO_NUM  13
-
-#elif defined(CAMERA_MODEL_ESP32_CAM_BOARD)
-// The 18 pin header on the board has Y5 and Y3 swapped
-#define USE_BOARD_HEADER 0
-#define PWDN_GPIO_NUM    32
-#define RESET_GPIO_NUM   33
-#define XCLK_GPIO_NUM    4
-#define SIOD_GPIO_NUM    18
-#define SIOC_GPIO_NUM    23
-
-#define Y9_GPIO_NUM 36
-#define Y8_GPIO_NUM 19
-#define Y7_GPIO_NUM 21
-#define Y6_GPIO_NUM 39
-#if USE_BOARD_HEADER
-#define Y5_GPIO_NUM 13
-#else
-#define Y5_GPIO_NUM 35
-#endif
-#define Y4_GPIO_NUM 14
-#if USE_BOARD_HEADER
-#define Y3_GPIO_NUM 35
-#else
-#define Y3_GPIO_NUM 13
-#endif
-#define Y2_GPIO_NUM    34
-#define VSYNC_GPIO_NUM 5
-#define HREF_GPIO_NUM  27
-#define PCLK_GPIO_NUM  25
-
-#elif defined(CAMERA_MODEL_ESP32S3_CAM_LCD)
-#define PWDN_GPIO_NUM  -1
-#define RESET_GPIO_NUM -1
-#define XCLK_GPIO_NUM  40
-#define SIOD_GPIO_NUM  17
-#define SIOC_GPIO_NUM  18
-
-#define Y9_GPIO_NUM    39
-#define Y8_GPIO_NUM    41
-#define Y7_GPIO_NUM    42
-#define Y6_GPIO_NUM    12
-#define Y5_GPIO_NUM    3
-#define Y4_GPIO_NUM    14
-#define Y3_GPIO_NUM    47
-#define Y2_GPIO_NUM    13
-#define VSYNC_GPIO_NUM 21
-#define HREF_GPIO_NUM  38
-#define PCLK_GPIO_NUM  11
-
-#elif defined(CAMERA_MODEL_ESP32S2_CAM_BOARD)
-// The 18 pin header on the board has Y5 and Y3 swapped
-#define USE_BOARD_HEADER 0
-#define PWDN_GPIO_NUM    1
-#define RESET_GPIO_NUM   2
-#define XCLK_GPIO_NUM    42
-#define SIOD_GPIO_NUM    41
-#define SIOC_GPIO_NUM    18
-
-#define Y9_GPIO_NUM 16
-#define Y8_GPIO_NUM 39
-#define Y7_GPIO_NUM 40
-#define Y6_GPIO_NUM 15
-#if USE_BOARD_HEADER
-#define Y5_GPIO_NUM 12
-#else
-#define Y5_GPIO_NUM 13
-#endif
-#define Y4_GPIO_NUM 5
-#if USE_BOARD_HEADER
-#define Y3_GPIO_NUM 13
-#else
-#define Y3_GPIO_NUM 12
-#endif
-#define Y2_GPIO_NUM    14
-#define VSYNC_GPIO_NUM 38
-#define HREF_GPIO_NUM  4
-#define PCLK_GPIO_NUM  3
-
-#elif defined(CAMERA_MODEL_ESP32S3_EYE)
-#define PWDN_GPIO_NUM  -1
-#define RESET_GPIO_NUM -1
-#define XCLK_GPIO_NUM  15
-#define SIOD_GPIO_NUM  4
-#define SIOC_GPIO_NUM  5
-
-#define Y2_GPIO_NUM 11
-#define Y3_GPIO_NUM 9
-#define Y4_GPIO_NUM 8
-#define Y5_GPIO_NUM 10
-#define Y6_GPIO_NUM 12
-#define Y7_GPIO_NUM 18
-#define Y8_GPIO_NUM 17
-#define Y9_GPIO_NUM 16
-
-#define VSYNC_GPIO_NUM 6
-#define HREF_GPIO_NUM  7
-#define PCLK_GPIO_NUM  13
-
-#elif defined(CAMERA_MODEL_DFRobot_FireBeetle2_ESP32S3) || defined(CAMERA_MODEL_DFRobot_Romeo_ESP32S3)
-#define PWDN_GPIO_NUM  -1
-#define RESET_GPIO_NUM -1
-#define XCLK_GPIO_NUM  45
-#define SIOD_GPIO_NUM  1
-#define SIOC_GPIO_NUM  2
-
-#define Y9_GPIO_NUM    48
-#define Y8_GPIO_NUM    46
-#define Y7_GPIO_NUM    8
-#define Y6_GPIO_NUM    7
-#define Y5_GPIO_NUM    4
-#define Y4_GPIO_NUM    41
-#define Y3_GPIO_NUM    40
-#define Y2_GPIO_NUM    39
-#define VSYNC_GPIO_NUM 6
-#define HREF_GPIO_NUM  42
-#define PCLK_GPIO_NUM  5
-
-#else
-#error "Camera model not selected"
-#endif
+  if (psramFound()) {
+    Serial.printf("PSRAM size: %d bytes\n", ESP.getPsramSize());
+    config.jpeg_quality = 10;
+    config.fb_count = 2;
+    config.grab_mode = CAMERA_GRAB_LATEST;
+  } else {
+    // Limit the frame size when PSRAM is not available
+    Serial.println("PSRAM not found!");
+    config.frame_size = FRAMESIZE_SVGA;
+    config.fb_location = CAMERA_FB_IN_DRAM;
+  }
+  
+  return config;
+}
